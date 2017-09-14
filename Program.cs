9219040e171a01280f3b4b78d3a6cc4b2d8cc085
Andrej <andrei-587@mail.ru>
2017-09-14 10:56:08 +0600

    Convert string file into KeyValue file where Key is a symbol and Value is symbol count COMPLETE!

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Zippo
 {
@@ -10,6 +9,42 @@ namespace Zippo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the file path (preferably .txt format and with russians symbols): ");
+            string filePath = Console.ReadLine();
+            Console.WriteLine("Reading...");
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+            byte[] array = new byte[(int)stream.Length];
+            if (stream.Read(array, 0, (int)stream.Length) == 0) Console.WriteLine("Reading successfull complete!");
+            else
+            {
+                Console.WriteLine("Reading complete with errors...");
+                return;
+            }
+            stream.Close();
+            string file = Encoding.UTF8.GetString(array);
+            Console.WriteLine("Counting a symbols...");
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            int size = 0;
+            for (int i = 0; i < file.Length; ++i)
+            {
+                int symbValue;
+                if (int.TryParse(file[i].ToString(), out symbValue)) size += 1;
+                else size += 2;
+                for (int j = 0; j < dictionary.Count; ++j)
+                {
+                    int value = 1;
+                    if (dictionary.TryGetValue(file[i], out value))
+                    {
+                        value++;
+                        dictionary.Remove(file[i]);
+                    }
+                    dictionary.Add(file[i], value);
+                }
+            }
+            Console.WriteLine("Counting successfull complete! Approximate file size: "+size+" bytes");
+            Console.WriteLine("Zipping...");
+
+            Console.ReadKey();
         }
     }
 }
