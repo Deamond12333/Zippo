04bbaf12fb38e03900ed6ee753d25bf1eb7adb12
Andrej <andrei-587@mail.ru>
2017-09-21 13:06:10 +0600

    Creating a HuffmanTree was COMPLETE!

---
+++ HuffmanTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zippo
+{
+    class HuffmanTree
+    {
+
+    }
+
+    class HuffmanTreeNode
+    {
+        public char Symbol { get; set; }
+        public HuffmanTreeNode Parent { get; set; }
+        public HuffmanTreeNode LeftChild { get; set; }
+        public HuffmanTreeNode RightChild { get; set; }
+        public int Weight { get; set; }
+        public char Rotation { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,41 +13,100 @@ namespace Zippo
             Console.WriteLine("Enter the file path (preferably .txt format and with russians symbols): ");
             string filePath = Console.ReadLine();
             Console.WriteLine("Reading...");
-            FileStream stream = new FileStream(filePath, FileMode.Open);
-            byte[] array = new byte[(int)stream.Length];
-            if (stream.Read(array, 0, (int)stream.Length) == 0) Console.WriteLine("Reading successfull complete!");
-            else
+            byte[] array;
+            try
+            {
+                FileStream stream = new FileStream(filePath, FileMode.Open);
+                array = new byte[(int)stream.Length];
+                stream.Read(array, 0, (int)stream.Length);
+                stream.Close();
+                Console.Write("complete!");
+            }
+            catch
             {
                 Console.WriteLine("Reading complete with errors...");
+                Console.ReadKey();
                 return;
             }
-            stream.Close();
             string file = Encoding.UTF8.GetString(array);
             Console.WriteLine("Counting a symbols...");
             Dictionary<char, int> dictionary = new Dictionary<char, int>();
-            int size = 0;
             for (int i = 0; i < file.Length; ++i)
             {
-                int symbValue;
-                if (int.TryParse(file[i].ToString(), out symbValue)) size += 1;
-                else size += 2;
-                for (int j = 0; j < dictionary.Count; ++j)
-                {
-                    int value = 1;
-                    if (dictionary.TryGetValue(file[i], out value))
-                    {
-                        value++;
-                        dictionary.Remove(file[i]);
-                    }
-                    dictionary.Add(file[i], value);
-                }
+                if (dictionary.ContainsKey(file[i])) dictionary[file[i]]++;
+                else dictionary.Add(file[i], 1);
             }
-            Console.WriteLine("Counting symbols successfull complete! Approximate file size: "+size+" bytes");
-            Console.WriteLine("Counting probabilities...");
-            Dictionary<char, double> probability = new Dictionary<char, double>();
-            foreach(var symbol in dictionary.OrderBy(symbol => symbol.Value)) probability.Add(symbol.Key, symbol.Value / dictionary.Count);
+            Console.Write("complete!");
+            Console.WriteLine("Creating a Huffman tree...");
+            List<HuffmanTreeNode> nodes = new List<HuffmanTreeNode>();
+            foreach(KeyValuePair<char, int> p in dictionary)
+            {
+                HuffmanTreeNode node = new HuffmanTreeNode();
+                node.Symbol = p.Key;
+                node.Weight = p.Value;
+                nodes.Add(node);
+            }
+            while (nodes.Count > 1)
+            {
+                nodes = nodes.OrderByDescending(n => n.Weight).ToList();
+                HuffmanTreeNode parentNode = new HuffmanTreeNode();
+                parentNode.LeftChild = nodes[0];
+                parentNode.LeftChild.Rotation = '0';
+                parentNode.RightChild = nodes[1];
+                parentNode.RightChild.Rotation = '1';
+                parentNode.Weight = parentNode.LeftChild.Weight + parentNode.RightChild.Weight;
+                nodes.RemoveAt(0);
+                nodes.RemoveAt(0);
+                nodes.Add(parentNode);
+            }
+            Console.Write("complete!");
+            Console.WriteLine("Creating output file...");
 
             Console.ReadKey();
         }
     }
 }
+/*
+            List<BiBranch> branches = new List<BiBranch>();
+            foreach (char symbol in probability.Keys)
+            {
+                BiLeaf leaf = new BiLeaf();
+                leaf.symbol = symbol;
+                leaf.nodeLevel = 0;
+                branches.Add(leaf);
+            }
+            int iterator = 0;
+            short levelID = 1, branchID = 1;
+            bool binValue = true;
+            BiBranch newBranch = new BiBranch();
+            foreach (var branch in branches)
+            {
+                if (levelID == 1)
+                {
+                    if (branch.GetType() == typeof(BiLeaf))
+                    {
+                        if (branchID < 3)
+                        {
+                            branch.lastBranch = newBranch;
+                            branchID++;
+                        }
+                        else
+                        {
+                            //branches.Add(newBranch);
+                            newBranch = new BiBranch();
+                            branchID = 1;
+                        }
+                        iterator++;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+
+                }
+
+            }
+*/
